8495dd4ad9be85bbadc2c2b738c72a95fe8f2472
Gucci Fan <gucc@umich.edu>
2022-08-06 12:32:57 -0400

    updated index.html and script.js for funsies

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "what's up nerds i'm writing on the internet";
 document.querySelector("body").appendChild(h2);
